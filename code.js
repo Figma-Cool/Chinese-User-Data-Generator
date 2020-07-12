9965efa95976574a27595f2e4010e0ad168f98b6
yancymin <senseslofter@gmail.com>
2020-07-12 14:47:38 +0800

    Initial commit

---
+++ code.js
@@ -0,0 +1,36 @@
+figma.showUI(__html__, { width: 250, height: 235 });
+
+let selectionArr = figma.currentPage.selection;
+figma.ui.postMessage({
+    selectionArr: figma.currentPage.selection.length
+})
+
+figma.on('selectionchange', () => {
+    selectionArr = []
+    if (figma.currentPage.selection.length > 0) {
+        selectionArr = []
+        selectionArr = figma.currentPage.selection;
+        figma.ui.postMessage({
+            selectionArr: selectionArr.length
+        })
+    }
+})
+
+figma.ui.onmessage = msg => {
+    async function nodeFont(nodeItem, index) {
+        await figma.loadFontAsync(nodeItem.fontName)
+        msg.name ? nodeItem.characters = msg.name[index] : null
+        msg.age ? nodeItem.characters = msg.age[index] : null
+        msg.gender ? nodeItem.characters = msg.gender[index] : null
+        msg.address ? nodeItem.characters = msg.address[index] : null
+        msg.job ? nodeItem.characters = msg.job[index] : null
+        msg.phone ? nodeItem.characters = msg.phone[index] : null
+        msg.mail ? nodeItem.characters = msg.mail[index] : null
+        msg.province ? nodeItem.characters = msg.province[index] : null
+        msg.city ? nodeItem.characters = msg.city[index] : null
+    }
+
+    selectionArr.forEach((item, index) => {
+        nodeFont(item, index)
+    })
+};
